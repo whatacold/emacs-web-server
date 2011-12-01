91da22ad1a9657456fe0a25e7f90d03945b4ec70
Christopher Wellons <mosquitopsu@gmail.com>
2011-12-01 16:21:59 -0500

    Switch to the UNLICENSE (public domain).

--- httpd.el
+++ httpd.el
@@ -1,21 +1,6 @@
 ;;; httpd.el --- HTTP/1.0 web server for emacs
 
-;; Copyright (C) 2009 Christopher Wellons <mosquitopsu@gmail.com>
-
-;; This file is not part of GNU Emacs.
-
-;; This file is free software; you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation; either version 2, or (at your option)
-;; any later version.
-
-;; This file is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;; GNU General Public License for more details.
-
-;; You should have received a copy of the GNU General Public License
-;; along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
+;; This is free and unencumbered software released into the public domain.
 
 ;;; Commentary:
 
